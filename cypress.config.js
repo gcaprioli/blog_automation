d3e6870887fc3d95456519bf5ffe588f7f4c3473
gcaprioli <gih.caprioli@gmail.com>
2024-09-22 21:20:22 -0300

    Revisão de escritas e criação do arquivo README.

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    // Durante a execução dos testes, percebi que há um fetch no Analytics que atrapalha os testes, então optei por bloquear
-    /*blockHosts: [
-      'www.google-analytics.com',   // Bloqueia o Google Analytics
-      'analytics.google.com',       // Bloqueia Google Analytics coleta
-      '*.google-analytics.com',     // Bloqueia o Google Analytics
-    ],*/
-
-    setupNodeEvents(on, config) {
+  e2e: {setupNodeEvents(on, config) {
       
     },
   },
--- cypress/e2e/blog_pesquisa.cy.js
+++ cypress/e2e/blog_pesquisa.cy.js
@@ -1,31 +1,29 @@
-//Rever escritas de "describe" e "it"
 describe('Validação de busca no blog', () => {
   beforeEach(() => {
     cy.clearCookies()
     cy.clearLocalStorage()
     cy.visit('https://blogdoagi.com.br')
   })
-
   
-  it('Tem campo de pesquisa', () => {
+  it('Verifica campo de pesquisa', () => {
     cy.get('.ast-search-icon').should('exist')  
   })
 
-  it('Pesquisa termo válido', () => {
+  it('Pesquisa termo simples e válido', () => {
     const termoPesquisa = "banco"
     cy.realizarPesquisa(termoPesquisa)
     cy.checarTopoPesquisa(termoPesquisa)
     cy.checarResultadosPesquisa(true, termoPesquisa)     
   })
 
-  it('Pesquisa termo inválido', () => {
+  it('Pesquisa termo simples e inválido', () => {
     const termoPesquisa = "sopro"
     cy.realizarPesquisa(termoPesquisa)
     cy.checarTopoPesquisa(termoPesquisa)
     cy.checarResultadosPesquisa(false, termoPesquisa)
   })
 
-  //Percebi que quando não digitamos nada e damos {enter}, a pesquisa ainda assim funciona, aparentemente trazendo os resultados ordenados dos mais recentes aos mais antigos
+  //Percebi que quando abrimos o campo de pesquisa e, sem digitar nada, apertamos {enter}, a pesquisa ainda assim funciona, aparentemente trazendo os resultados ordenados dos mais recentes aos mais antigos
   it('Pesquisa em branco', () => {
     const termoPesquisa = "" 
     cy.realizarPesquisa(termoPesquisa)
